95f4e51adf24bc7f103dee5732b7b682c5d785aa
luna111122 <hyewonkwon100@gmail.com>
2025-12-30 16:04:05 +0900

    init: spring boot project

---
+++ src/main/java/com/kickmate/kickmate/KickmateApplication.java
@@ -0,0 +1,13 @@
+package com.kickmate.kickmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kickmate/kickmate/KickmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kickmate.kickmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
