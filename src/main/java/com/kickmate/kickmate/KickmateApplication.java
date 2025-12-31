c8b6e751c9726f25cb02b0e24f29b976b8087cf0
luna111122 <hyewonkwon100@gmail.com>
2025-12-31 16:15:25 +0900

    chore: 주석 처리

--- src/main/java/com/kickmate/kickmate/KickmateApplication.java
+++ src/main/java/com/kickmate/kickmate/KickmateApplication.java
@@ -3,6 +3,7 @@ package com.kickmate.kickmate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//시작 경로
 @SpringBootApplication
 public class KickmateApplication {
 
