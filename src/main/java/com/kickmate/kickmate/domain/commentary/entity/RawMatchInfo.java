cab6bdeba2404b7f51e42ca28768087665a86e8f
luna111122 <hyewonkwon100@gmail.com>
2026-01-04 18:16:18 +0900

    chore: 패키지 위치 변경

--- src/main/java/com/kickmate/kickmate/domain/commentary/entity/RawMatchInfo.java
+++ src/main/java/com/kickmate/kickmate/domain/commentary/entity/RawMatchInfo.java
@@ -1,10 +1,9 @@
-package com.kickmate.kickmate;
+package com.kickmate.kickmate.domain.commentary.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.Instant;
 
